8192b571e696e6773fc2c76b687ff6109bb36c89
Ekaterina Buryakova <eo.popova@yandex.ru>
2025-05-21 22:33:18 +0300

    create ads models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+
+class Ad(models.Model):
+    CONDITION_CHOICES = (
+        ('new', 'новый'),
+        ('used', 'б/у'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    description = models.CharField(max_length=250)
+    image_url = models.ImageField(upload_to='uploads/')
+    category = models.ManyToManyField(Category)
+    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, default='NW')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ExchangeProposal(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'ожидает'),
+        ('accept', 'принята'),
+        ('decline', 'отклонена'),
+    )
+    ad_sender = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='sent_proposals')
+    ad_receiver = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='received_proposals')
+    comment = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
